3dcd0024ad72c42ee28236eca9b76af8bb0a900a
Ahmed Mukhtar <mukhtar1100@hotmail.com>
2024-10-06 09:23:44 +0200

    Fixtures & Data Driven Test

--- cypress/e2e/webdriveruni/contact-us.js
+++ cypress/e2e/webdriveruni/contact-us.js
@@ -1,28 +1,33 @@
 /// <reference types="Cypress" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
+  before(() => {
+    cy.fixture("example").then((data) => {
+      globalThis.data = data;
+    });
+  });
   it.only("Should be able to submit a successfull submission contact us form", () => {
-    cy.document().should("have.a.property", "charset").and("eq", "UTF-8")
+    cy.document().should("have.a.property", "charset").and("eq", "UTF-8");
     // cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
-    cy.visit("https://www.webdriveruniversity.com")
-    cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true })
+    cy.visit("https://www.webdriveruniversity.com");
+    cy.get("#contact-us").invoke("removeAttr", "target").click({ force: true });
 
-    // cy.get("#contact-us").click({ force: true })
-    // cy.get('[name="first_name"]').type("Ahmed")
-    // cy.get('[name="last_name"]').type("Muhammed")
-    // cy.get('[name="email"]').type("mukhtar1100@hotmail.com")
-    // cy.get("textarea.feedback-input").type("This is a feeedback")
-    // cy.get('[type="submit"]').click()
-  })
+    // cy.get("#contact-us").click({ force: true });
+    cy.get('[name="first_name"]').type(data.first_name);
+    cy.get('[name="last_name"]').type(data.last_name);
+    cy.get('[name="email"]').type(data.email);
+    cy.get("textarea.feedback-input").type("This is a feeedback");
+    cy.get('[type="submit"]').click();
+  });
 
   it("Should not be able to submit a successfull submission contact us form as all fields required", () => {
     // cy.visit("https://www.webdriveruniversity.com/Contact-Us/contactus.html")
-    cy.visit("https://www.webdriveruniversity.com")
-    cy.get("#contact-us").click({ force: true })
-    cy.get('[name="first_name"]').type("Ahmed")
-    cy.get('[name="last_name"]').type("Muhammed")
-    cy.get("textarea.feedback-input").type("This is a feeedback")
-    cy.get('[type="submit"]').click()
-    cy.get("body").contains("Error: all fields are required")
-  })
-})
+    cy.visit("https://www.webdriveruniversity.com");
+    cy.get("#contact-us").click({ force: true });
+    cy.get('[name="first_name"]').type(data.first_name);
+    cy.get('[name="last_name"]').type(data.last_name);
+    cy.get("textarea.feedback-input").type("This is a feeedback");
+    cy.get('[type="submit"]').click();
+    cy.get("body").contains("Error: all fields are required");
+  });
+});
