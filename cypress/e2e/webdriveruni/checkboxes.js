da1fdc703c2c624e9bec2dfd296850d658768e21
Ahmed Mukhtar <mukhtar1100@hotmail.com>
2024-10-06 08:44:11 +0200

    Cypress hooks

--- cypress/e2e/webdriveruni/checkboxes.js
+++ cypress/e2e/webdriveruni/checkboxes.js
@@ -1,42 +1,33 @@
 /// <reference types="Cypress" />
 
 describe("Check & Checkboxes", () => {
-  it("Validate check AND Checkboxes", () => {
-    cy.visit("https://www.webdriveruniversity.com")
+  beforeEach(() => {
+    cy.visit("https://www.webdriveruniversity.com");
     cy.get("#dropdown-checkboxes-radiobuttons")
       .invoke("removeAttr", "target")
-      .click({ force: true })
-
+      .click({ force: true });
+  });
+  it("Validate check AND Checkboxes", () => {
     // cy.get("#checkboxes > :nth-child(1) > input").check()
-    cy.get("#checkboxes > :nth-child(1) > input").as("option-1")
+    cy.get("#checkboxes > :nth-child(1) > input").as("option-1");
     // cy.get("@option-1").check()
-    cy.get("@option-1").check().should("be.checked")
-  })
+    cy.get("@option-1").check().should("be.checked");
+  });
 
   it("Validate uncheck AND Checkboxes", () => {
-    cy.visit("https://www.webdriveruniversity.com")
-    cy.get("#dropdown-checkboxes-radiobuttons")
-      .invoke("removeAttr", "target")
-      .click({ force: true })
-
     // cy.get("#checkboxes > :nth-child(1) > input").check()
-    cy.get("#checkboxes > :nth-child(5) > input").as("option-5")
+    cy.get("#checkboxes > :nth-child(5) > input").as("option-5");
 
-    cy.get("@option-5").uncheck()
-    cy.get("@option-5").should("not.be.checked")
-  })
+    cy.get("@option-5").uncheck();
+    cy.get("@option-5").should("not.be.checked");
+  });
 
   it.only("One Command to uncheck multiple Checkboxes", () => {
-    cy.visit("https://www.webdriveruniversity.com")
-    cy.get("#dropdown-checkboxes-radiobuttons")
-      .invoke("removeAttr", "target")
-      .click({ force: true })
-
     // cy.get("#checkboxes > :nth-child(1) > input").check()
-    cy.get('[type="checkbox"]').as("checkboxes")
+    cy.get('[type="checkbox"]').as("checkboxes");
 
-    cy.get("@checkboxes").check().should("be.checked")
+    cy.get("@checkboxes").check().should("be.checked");
 
-    cy.get("@checkboxes").uncheck().should("not.be.checked")
-  })
-})
+    cy.get("@checkboxes").uncheck().should("not.be.checked");
+  });
+});
--- cypress/e2e/webdriveruni/radio-buttons.js
+++ cypress/e2e/webdriveruni/radio-buttons.js
@@ -1,23 +1,20 @@
 /// <reference types="Cypress" />
 
 describe("Verify radio buttons with webdriveruni", () => {
-  it("check specific radio buttons", () => {
-    cy.visit("https://www.webdriveruniversity.com")
+  before(function () {
+    cy.visit("https://www.webdriveruniversity.com");
     cy.get("#dropdown-checkboxes-radiobuttons")
       .invoke("removeAttr", "target")
-      .click({ force: true })
-
-    cy.get("#radio-buttons").find("input[type='radio']").first().check()
-    cy.get("#radio-buttons").find("input[type='radio']").eq(4).check()
-  })
+      .click({ force: true });
+  });
+  it("check specific radio buttons", () => {
+    cy.get("#radio-buttons").find("input[type='radio']").first().check();
+    cy.get("#radio-buttons").find("input[type='radio']").eq(4).check();
+  });
 
   it.only("Validate state of specific radio buttons", () => {
-    cy.visit("https://www.webdriveruniversity.com")
-    cy.get("#dropdown-checkboxes-radiobuttons")
-      .invoke("removeAttr", "target")
-      .click({ force: true })
-    cy.get("[value='lettuce']").should("not.be.checked")
-    cy.get("[value='pumpkin']").should("be.checked")
-    cy.get("[value='cabbage']").should("be.disabled")
-  })
-})
+    cy.get("[value='lettuce']").should("not.be.checked");
+    cy.get("[value='pumpkin']").should("be.checked");
+    cy.get("[value='cabbage']").should("be.disabled");
+  });
+});
