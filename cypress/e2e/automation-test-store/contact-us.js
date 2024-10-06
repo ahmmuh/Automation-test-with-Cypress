381e5f11c63a37f8e006ef7c1fcba8e8a5117878
Ahmed Mukhtar <mukhtar1100@hotmail.com>
2024-10-06 09:40:26 +0200

    alias & fixtures

--- cypress/e2e/automation-test-store/contact-us.js
+++ cypress/e2e/automation-test-store/contact-us.js
@@ -2,17 +2,25 @@
 /// <reference types="@cypress/xpath" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
+  before(() => {
+    cy.fixture("userDetails").as("user");
+  });
   it("Should be able to submit a successfull submission contact us form", () => {
-    cy.visit("https://automationteststore.com/")
+    cy.visit("https://automationteststore.com/");
     cy.get(".info_links_footer > :nth-child(5) > a")
       .click()
       .then((linkText) => {
-        cy.log("Link Text: " + linkText.text())
-      })
-    cy.get("#ContactUsFrm_first_name").type("Abdinaasir Ahmed")
-    cy.get("#ContactUsFrm_email").type("abdinaasir@gmail.com")
+        cy.log("Link Text: " + linkText.text());
+      });
+
+    cy.get("@user").then((user) => {
+      cy.get("#ContactUsFrm_first_name").type(user.first_name);
+      cy.get("#ContactUsFrm_email").type(user.email);
+    });
+    cy.get("#ContactUsFrm_first_name").type("Abdinaasir Ahmed");
+    cy.get("#ContactUsFrm_email").type("abdinaasir@gmail.com");
     cy.get("#ContactUsFrm_enquiry").type(
       "You enquiry has been sent to the Company"
-    )
-  })
-})
+    );
+  });
+});
