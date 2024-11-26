8bc33765c694243fdd9e0702ae187e6dea1b25d6
Ahmed Mukhtar <mukhtar1100@hotmail.com>
2024-11-26 20:08:07 +0100

    A short Git instruction

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,24 @@
 const { defineConfig } = require("cypress");
 
+const fs = require("fs-extra");
+const path = require("path");
+
+function getConfigurationByFile(file) {
+  const pathToConfigFile = path.resolve("cypress/config", `${file}.json`);
+  if (!fs.existsSync(pathToConfigFile)) {
+    console.log("No custom config file was found ");
+    return {};
+  }
+  return fs.readJson(pathToConfigFile);
+}
+
 module.exports = defineConfig({
   projectId: "537tjz",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      const file = config.env.configFile || "";
+      return getConfigurationByFile(file);
     },
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx,feature}",
     chromeWebSecurity: false,
@@ -15,6 +29,10 @@ module.exports = defineConfig({
     viewportHeight: 550,
     viewportWidth: 750,
     baseUrl: "https://www.webdriveruniversity.com",
+    reporter: "cypress-multi-reporters",
+    reporterOptions: {
+      configFile: "reporter-config.json",
+    },
     env: {
       first_name: "Sarah",
       webdriveruni_homePage: "https://www.webdriveruniversity.com",
